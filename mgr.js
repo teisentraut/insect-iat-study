fcbaf43f8c9fde63383f5f207d87d80734966b4b
teisentraut <travis.eisentraut@gmail.com>
2026-07-11 20:15:27 -0400

    Refactor preiat_instructions properties in mgr.js

--- mgr.js
+++ mgr.js
@@ -73,12 +73,10 @@ define(['managerAPI',
         }],
 
         preiat_instructions: [{
-    inherit: 'instructions',
+    type: 'message',
     name: 'preiat_instructions',
     templateUrl: 'preiat_instructions.jst',
-    title: 'IAT Instructions',
-    header: 'Insect–Mammal Implicit Association Test',
-    buttonText: 'Start IAT'
+    keys: [' ', 13]
 }],
 
         explicits: [{
